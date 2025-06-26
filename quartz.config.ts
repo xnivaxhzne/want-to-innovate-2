ae7f44ae6514aa834a0770dd407a0726f8497a3c
Kavinkumar <kavinkumar1543884@gmail.com>
2025-06-26 09:11:51 +0530

    Update quartz layout

--- quartz.config.ts
+++ quartz.config.ts
@@ -56,9 +56,9 @@ const config: QuartzConfig = {
   plugins: {
     transformers: [
       Plugin.FrontMatter(),
-      Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "git", "filesystem"],
-      }),
+      // Plugin.CreatedModifiedDate({
+      //   priority: ["frontmatter", "git", "filesystem"],
+      // }),
       Plugin.SyntaxHighlighting({
         theme: {
           light: "github-light",
