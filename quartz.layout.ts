979f6d589a91cc5166a3c919d39419e4a38f9f8a
xnivaxhzne <112480281+xnivaxhzne@users.noreply.github.com>
2025-07-14 22:12:54 +0530

    Hide explorer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    // Component.Explorer(),
   ],
   right: [
     Component.Graph(),
@@ -62,7 +62,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    // Component.Explorer(),
   ],
   right: [],
 }
