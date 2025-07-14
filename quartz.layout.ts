a5e86fb8f34cbf9727a0fea4c516c5722957bf54
xnivaxhzne <112480281+xnivaxhzne@users.noreply.github.com>
2025-07-14 22:23:04 +0530

    Hide reading time in the breadcrumb

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -22,7 +22,9 @@ export const defaultContentPageLayout: PageLayout = {
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
+    Component.ContentMeta({
+      showReadingTime: false
+    }),
     Component.TagList(),
   ],
   left: [
